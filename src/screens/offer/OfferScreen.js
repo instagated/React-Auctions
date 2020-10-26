00614ce00e3ecb9aa2e5b0fc9f1ce4793c906be0
Thorben <38816229+tklein1801@users.noreply.github.com>
2020-10-26 17:38:39 +0100

    :boom: Get real-time changes using Firestore

--- src/screens/offer/OfferScreen.js
+++ src/screens/offer/OfferScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Col, Row, Button, InputGroup, FormControl } from "react-bootstrap";
-import Auction from "../../Auction";
+import { Col, Button, InputGroup, FormControl } from "react-bootstrap";
 import Loader from "../../components/Loader";
+import Firebase, { firestore } from "../../Firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./OfferScreen.scss";
 
@@ -11,159 +11,232 @@ export default class OfferScreen extends Component {
     this.state = {
       loading: true,
       offerId: window.location.href.split("/")[4],
-      offer: null,
-      thumbnail: null,
-      seller: null,
-      countdown: null,
     };
-  }
 
-  componentDidMount() {
-    new Auction().getOffer(this.state.offerId).then((offer) => {
-      new Auction().getSeller(offer.seller).then((seller) => {
-        let user = seller;
-        user.seller = offer.seller;
-        user.avatar = new Auction().getAvatar(seller.username);
-        this.setState({
-          offer: offer,
-          thumbnail: offer.images.thumbnail,
-          seller: seller,
-          loading: false,
-        });
-
-        let countdown = setInterval(() => {
-          let now = Date.now() / 1000,
-            expiresAt = offer.expiresAt,
-            diff = expiresAt - now;
-          if (diff > 0) {
-            var d = Math.floor(diff / 86400),
-              h = Math.floor((diff - d * 86400) / 3600),
-              h = h < 10 ? `0${h}` : h,
-              m = Math.floor((diff - d * 86400 - h * 3600) / 60),
-              m = m < 10 ? `0${m}` : m,
-              s = Math.floor(diff - d * 86400 - h * 3600 - m * 60),
-              s = s < 10 ? `0${s}` : s;
-            if (d !== 0) {
-              d > 1
-                ? this.setState({ countdown: `Verbleibende Zeit: ${d} Tage & ${h}:${m}:${s}` })
-                : this.setState({ countdown: `Verbleibende Zeit: ${d} Tag & ${h}:${m}:${s}` });
-            } else {
-              this.setState({ countdown: `Verbleibende Zeit: ${h}:${m}:${s}` });
-            }
-          } else {
-            clearInterval(countdown);
-            this.setState({ countdown: "Das Angebot ist nicht mehr verfügbar" });
-          }
-        }, 1000);
-      });
+    // Check if the user is signed in via Firebase
+    Firebase.auth().onAuthStateChanged((user) => {
+      user
+        ? this.setState({ authentificated: true, user: user })
+        : this.setState({ authentificated: false });
     });
   }
 
+  componentDidMount() {
+    let { offerId } = this.state,
+      temp = [];
+
+    // Get the offer from the database
+    firestore
+      .collection("offers")
+      .doc(offerId)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          let offer = doc.data();
+          offer.id = offerId;
+
+          let countdown = setInterval(() => {
+            let now = Date.now() / 1000,
+              expiresAt = offer.expiresAt.seconds,
+              diff = expiresAt - now;
+            if (diff > 0) {
+              var d = Math.floor(diff / 86400),
+                h = Math.floor((diff - d * 86400) / 3600),
+                h = h < 10 ? `0${h}` : h,
+                m = Math.floor((diff - d * 86400 - h * 3600) / 60),
+                m = m < 10 ? `0${m}` : m,
+                s = Math.floor(diff - d * 86400 - h * 3600 - m * 60),
+                s = s < 10 ? `0${s}` : s;
+              if (d !== 0) {
+                d > 1
+                  ? this.setState({ countdown: `Verbleibende Zeit: ${d} Tage & ${h}:${m}:${s}` })
+                  : this.setState({ countdown: `Verbleibende Zeit: ${d} Tag & ${h}:${m}:${s}` });
+              } else {
+                this.setState({ countdown: `Verbleibende Zeit: ${h}:${m}:${s}` });
+              }
+            } else {
+              clearInterval(countdown);
+              this.setState({ countdown: "Das Angebot ist nicht mehr verfügbar" });
+            }
+          }, 1000);
+
+          temp.push(offer);
+          offer.seller.get().then((user) => {
+            temp.push(user);
+            this.setState({
+              found: true,
+              offer: temp[0],
+              seller: temp[1],
+              thumbnail: temp[0].images.thumbnail,
+              loading: false,
+            });
+          });
+        } else {
+          this.setState({ loading: false, found: false });
+        }
+      });
+
+    // Get real-tiem document changes & update the state
+    firestore
+      .collection("offers")
+      .doc(offerId)
+      .onSnapshot((doc) => {
+        if (doc.exists) {
+          let offer = doc.data();
+          offer.id = offerId;
+          this.setState({ offer: offer });
+        }
+      });
+  }
+
   render() {
-    if (this.state.loading === true) {
+    let {
+      loading,
+      found,
+      authentificated,
+      user,
+      offerId,
+      offer,
+      seller,
+      thumbnail,
+      countdown,
+    } = this.state;
+
+    if (loading) {
       return <Loader />;
     } else {
-      let offer = this.state.offer;
-      return (
-        <section className="offer">
-          <div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
-              <Col className="px-0 px-md-3" xs={12} md={8} ld={8}>
-                <main>
-                  <div className="w-100 mb-3">
-                    <img className="thumbnail" src={this.state.thumbnail} alt="Produktbild" />
-                  </div>
-                  <div className="image-container mb-3">
-                    <a
-                      className="image"
-                      onClick={() => {
-                        this.setState({ thumbnail: offer.images.thumbnail });
-                      }}
-                    >
-                      <img src={offer.images.thumbnail} alt="Produktbild" />
-                    </a>
-                    {offer.images.product.map((image, index) => {
-                      return (
-                        <a
-                          className="image"
-                          onClick={() => {
-                            this.setState({ thumbnail: image });
-                          }}
-                          key={index}
-                        >
-                          <img src={image} alt="Produktbild" />
-                        </a>
-                      );
-                    })}
-                  </div>
-                </main>
-              </Col>
-              <Col className="px-0 px-md-3" xs={12} md={4} lg={4}>
-                <aside>
-                  <div className="d-flex bg-light mb-3 p-3" style={{ borderRadius: 12 }}>
-                    <p className="font-weight-bold mb-0">
-                      <img
-                        style={{ width: "2.5rem", height: "auto", marginRight: 3 }}
-                        className="rounded-circle shadow-md"
-                        src={this.state.seller.avatar}
-                        alt="Profilbild"
-                      />{" "}
-                      {this.state.seller.username}
-                    </p>
-                    <Button
-                      className="ml-auto px-3"
-                      variant="success"
-                      size="sm"
-                      style={{ borderRadius: 12 }}
-                      disabled={true}
-                    >
-                      Kontaktieren
-                    </Button>
-                  </div>
-
-                  <div className="bg-light p-3" style={{ borderRadius: 12 }}>
-                    <h3 className="font-weight-bold">{offer.name}</h3>
-                    <h5 className="font-weight-bold">{offer.price.toLocaleString(undefined)} €</h5>
-                    <p className="mb-0">{this.state.countdown}</p>
-                    {offer.type === 1 ? (
-                      <InputGroup className="mb-2">
-                        <FormControl
-                          className="bg-light"
-                          placeholder="Gebot"
-                          style={{ borderTopLeftRadius: 12, borderBottomLeftRadius: 12 }}
-                        />
-                        <InputGroup.Append>
-                          <InputGroup.Text>.00 €</InputGroup.Text>
-                        </InputGroup.Append>
-                        <InputGroup.Append>
-                          <Button
-                            variant="success"
-                            style={{ borderTopRightRadius: 12, borderBottomRightRadius: 12 }}
+      // FIXME Get the avatar from the instead of using the DulliAG logo
+      if (found) {
+        return (
+          <section className="offer">
+            <div>
+              <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+                <Col className="px-0 px-md-3" xs={12} md={8} lg={8}>
+                  <main>
+                    <div className="w-100 mb-3">
+                      <img className="thumbnail" src={thumbnail} alt="Vorschaubild" />
+                    </div>
+                    <div className="image-container mb-3">
+                      <a
+                        className="image"
+                        onClick={() => this.setState({ thumbnail: offer.images.thumbnail })}
+                      >
+                        <img src={offer.images.thumbnail} alt="Vorschaubild" />
+                      </a>
+                      {offer.images.product.map((image, index) => {
+                        return (
+                          <a
+                            className="image"
+                            onClick={() => this.setState({ thumbnail: image })}
+                            key={index}
                           >
-                            Bieten
-                          </Button>
-                        </InputGroup.Append>
-                      </InputGroup>
-                    ) : (
-                      <Button className="w-100" variant="success" style={{ borderRadius: 12 }}>
-                        Kaufen
+                            <img src={image} alt="Produktbild" />
+                          </a>
+                        );
+                      })}
+                    </div>
+                  </main>
+                </Col>
+                <Col className="px-0 px-md-3" xs={12} md={4} lg={4}>
+                  <aside>
+                    <div className="d-flex bg-light rounded mb-3 p-3">
+                      <p className="font-weight-bold mb-0">
+                        <img
+                          style={{ width: "2.5rem", height: "auto", marginRight: 3 }}
+                          className="rounded-circle shadow-md"
+                          src={"https://files.dulliag.de/web/images/logo.jpg"}
+                          alt="Profilbild"
+                        />{" "}
+                        {seller.data().username}
+                      </p>
+                      <Button
+                        className="ml-auto rounded px-3"
+                        variant="success"
+                        size="sm"
+                        disabled={true}
+                      >
+                        Kontaktieren
                       </Button>
-                    )}
-                    <p className="font-weight-bold mb-0">Beschreibung</p>
-                    <p className="mb-0">{offer.description}</p>
-                  </div>
-                </aside>
-              </Col>
+                    </div>
+
+                    <div className="bg-light rounded p-3">
+                      <h3 className="font-weight-bold">{offer.name}</h3>
+                      <h5 className="font-weight-bold">
+                        {offer.price.toLocaleString(undefined)} €
+                      </h5>
+                      <p className="mb-0">{countdown}</p>
+                      {authentificated && user.uid !== seller.id ? (
+                        offer.type === 1 ? (
+                          <form
+                            ref={(target) => (this.formRef = target)}
+                            onSubmit={(event) => {
+                              event.preventDefault();
+                              const { bid } = event.target.elements;
+                              // TODO Push an string with the current winner of the auction
+                              firestore
+                                .collection("offers")
+                                .doc(offer.id)
+                                .update({
+                                  price: parseInt(bid.value),
+                                });
+                              this.formRef.reset();
+                            }}
+                          >
+                            <InputGroup className="mb-2">
+                              <FormControl
+                                type="number"
+                                pattern="numeric"
+                                min={offer.price + 1}
+                                id="bid"
+                                className="bg-light"
+                                placeholder={
+                                  "Min. " + (offer.price + 1).toLocaleString(undefined) + " €"
+                                }
+                                style={{ borderTopLeftRadius: 5, borderBottomLeftRadius: 5 }}
+                              />
+                              <InputGroup.Append>
+                                <Button
+                                  type="submit"
+                                  variant="success"
+                                  className="px-3"
+                                  style={{ borderTopRightRadius: 5, borderBottomRightRadius: 5 }}
+                                  onClick={() => {
+                                    // TODO Push the uid from the current user to the offer & set expiresAt to 0
+                                  }}
+                                >
+                                  Bieten
+                                </Button>
+                              </InputGroup.Append>
+                            </InputGroup>
+                          </form>
+                        ) : (
+                          <Button className="w-100" variant="success" style={{ borderRadius: 5 }}>
+                            Kaufen
+                          </Button>
+                        )
+                      ) : null}
+                      <p className="font-weight-bold mb-0">Beschreibung</p>
+                      <p className="mb-0">{offer.description}</p>
+                    </div>
+                  </aside>
+                </Col>
+              </div>
             </div>
-          </div>
-        </section>
-      );
+          </section>
+        );
+      } else {
+        return (
+          <section>
+            <div>
+              <div className="bg-light mx-auto px-4 py-3 rounded">
+                <h3 className="font-weight-bold text-center mb-0">
+                  Das Angebot <i>{offerId}</i> wurde nicht gefunden
+                </h3>
+              </div>
+            </div>
+          </section>
+        );
+      }
     }
   }
 }
--- src/screens/offers/OffersScreen.js
+++ src/screens/offers/OffersScreen.js
@@ -3,52 +3,91 @@ import { Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Offer } from "../../components/Card";
 import Loader from "../../components/Loader";
+import { firestore } from "../../Firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class OffersScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    let temp = [];
+
+    firestore.collection("offers").onSnapshot((snapshot) => {
+      let changes = snapshot.docChanges();
+      changes.forEach((change) => {
+        let doc = change.doc,
+          data = doc.data(),
+          now = parseInt((Date.now() / 1000).toFixed(0));
+
+        if (change.type === "added") {
+          if (data.expiresAt > now) {
+            data.id = doc.id;
+            temp.push(data);
+          }
+        } else if (change.type === "removed") {
+          temp = temp.filter((offer) => {
+            return offer.id !== doc.id;
+          });
+        } else if (change.type === "modified") {
+          temp = temp.filter((offer) => {
+            return offer.id !== doc.id;
+          });
+          temp.push(data);
+        }
+      });
+      this.setState({ offers: temp, loading: false });
+    });
+  }
+
   render() {
-    let now = parseInt((Date.now() / 1000).toFixed(0));
-    return (
-      <section>
-        <div>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              flexWrap: "wrap",
-            }}
-          >
-            {this.props.offers.map((offer, index) => {
-              return offer.expiresAt > now ? (
-                <Col
-                  xs={12}
-                  md={6}
-                  lg={6}
-                  xl={3}
-                  key={index}
-                  className={
-                    this.props.offers.lenth > 4 ? "mb-3 px-0 px-md-3" : "mb-mb-3 px-0 px-md-3"
-                  }
-                >
-                  <div>
-                    <Link to={`/Angebot/${offer.id}`} style={{ textDecoration: "none" }}>
-                      <Offer
-                        type={offer.type}
-                        thumbnail={offer.images.thumbnail}
-                        name={offer.name}
-                        description={offer.description}
-                        price={offer.price}
-                      />
-                    </Link>
-                  </div>
-                </Col>
+    let { loading, offers } = this.state;
+
+    if (loading) {
+      return <Loader />;
+    } else {
+      return (
+        <section>
+          <div>
+            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+              {offers.length > 0 ? (
+                offers.map((offer, index) => {
+                  return (
+                    <Col
+                      xs={12}
+                      md={6}
+                      lg={6}
+                      xl={3}
+                      key={index}
+                      className={(offers.lenth < 4 ? "mb-md-0" : "", "mb-3 px-0 px-md-3")}
+                    >
+                      <div>
+                        <Link to={`/Angebot/${offer.id}`} style={{ textDecoration: "none" }}>
+                          <Offer
+                            type={offer.type}
+                            thumbnail={offer.images.thumbnail}
+                            name={offer.name}
+                            description={offer.description}
+                            price={offer.price}
+                          />
+                        </Link>
+                      </div>
+                    </Col>
+                  );
+                })
               ) : (
-                <Loader />
-              );
-            })}
+                <div className="bg-light mx-auto px-4 py-3 rounded">
+                  <h3 className="font-weight-bold text-center mb-0">Keine Angebote gefunden</h3>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
 }
