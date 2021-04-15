5ecfffc36b72cb90c6266412c2e0e5562eddc95d
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-04-16 00:05:08 +0200

    Update ApiHandler.js

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -1,4 +1,5 @@
 import { avatar } from "./config.json";
+import { firestore } from "./Firebase";
 
 class User {
   /**
@@ -9,9 +10,48 @@ class User {
   getAvatar(username) {
     return `https://eu.ui-avatars.com/api/?name=${username}&size=${avatar.size}&background=${avatar.background}&color=${avatar.color}`;
   }
+
+  async getUsername(userId) {
+    var userRef = await firestore.collection("user").doc(userId).get();
+    var userData = userRef.data();
+    var username = userData.username;
+    return username;
+  }
 }
 
-class Offer {}
+class Offer {
+  /**
+   * Update offers & user document
+   * @param {object} offer
+   * @param {object} user
+   * @returns {Promise}
+   */
+  buy(offer, user) {
+    return new Promise((res, rej) => {
+      firestore
+        .collection("offers")
+        .doc(offer)
+        .update({
+          bought: { uid: user.id, user: user, at: new Date() },
+        })
+        .then(() => {
+          firestore
+            .collection("user")
+            .doc(user)
+            .get()
+            .then((docRef) => {
+              let offerList = docRef.data().bought;
+              offerList.push(offer);
+              firestore.collection("user").doc(user).update({
+                bought: offerList,
+              });
+              res(offerList);
+            });
+        })
+        .catch((err) => rej(err));
+    });
+  }
+}
 
 class Auction extends Offer {}
 
