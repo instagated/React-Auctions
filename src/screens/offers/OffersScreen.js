264fe7bfb92177e23ccbab8eca06993956f89771
Thorben <38816229+tklein1801@users.noreply.github.com>
2021-04-22 23:32:46 +0200

    Update winner of auction after time runs out

--- src/screens/offers/OffersScreen.js
+++ src/screens/offers/OffersScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Offer as OfferAPI, Auction } from "../../ApiHandler";
 import { firestore } from "../../Firebase";
 // StyleSheets
 import { Col } from "react-bootstrap";
@@ -7,6 +8,9 @@ import { Offer } from "../../components/Offer";
 import Loader from "../../components/Loader";
 // StyleSheets
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const OfferHandler = new OfferAPI();
+const AuctionHandler = new Auction();
 
 export default class OffersScreen extends Component {
   constructor() {
@@ -17,7 +21,7 @@ export default class OffersScreen extends Component {
   }
 
   componentDidMount() {
-    let temp = [];
+    var temp = [];
     firestore
       .collection("offers")
       .orderBy("createdAt", "desc")
@@ -26,22 +30,37 @@ export default class OffersScreen extends Component {
         changes.forEach((change) => {
           const document = change.doc;
           const documentData = document.data();
+          const offerId = document.id;
           var now = parseInt((Date.now() / 1000).toFixed(0));
           var changeType = change.type;
+
           var gotBought = documentData.bought !== undefined;
+          var isExpired = documentData.expiresAt.seconds < now;
+          var isAuction = AuctionHandler.isAuction(documentData);
+          var receivedBids = documentData.bids !== undefined;
 
           if (changeType === "added") {
-            if (documentData.expiresAt.seconds > now && !gotBought) {
-              documentData.id = document.id;
+            if (!isExpired && !gotBought) {
+              documentData.id = offerId;
               temp.push(documentData);
+            } else if (isAuction && isExpired) {
+              // Check if there is an bid on this offer
+              // If an user have bid on this offer we're gonna update the documentDate.bought-object
+              if (receivedBids) {
+                AuctionHandler.getHighestBid(offerId)
+                  .then((bid) => {
+                    OfferHandler.buy(offerId, bid.user);
+                  })
+                  .catch((err) => console.error("ERROR:", err));
+              }
             }
           } else if (changeType === "removed") {
             temp = temp.filter((offer) => {
-              return offer.id !== document.id;
+              return offer.id !== offerId;
             });
           } else if (changeType === "modified") {
             temp = temp.filter((offer) => {
-              return offer.id !== document.id;
+              return offer.id !== offerId;
             });
             temp.push(documentData);
           }
